63deb1d5c1cfc5ad91d922fb830ba5f12b3747b1
soccerref07 <paulromano7@verizon.net>
2017-03-29 22:32:41 -0400

    Updated
    Updated, added files

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,37 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+package 'cohesity' do
+	action :install
+end
+
+cookbook_file "/etc/hostname.le0" do
+	source "hostname.le0"
+	mode "644"
+end
+
+cookbook_file "/etc/hostname.le1" do
+	source "hostname.le0"
+	mode "644"
+end
+
+cookbook_file "/etc/hostname.le2" do
+	source "hostname.le0"
+	mode "644"
+end
+
+cookbook_file "/etc/hostname.le3" do
+	source "hostname.le0"
+	mode "644"
+end
+
+cookbook_file "/etc/netmasks" do
+	source "netmasks"
+	mode "644"
+end
+
+cookbook_files "/etc/hosts" do
+	source "hosts"
+	mode "644"
+end
